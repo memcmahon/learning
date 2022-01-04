d6bc8108f8afaa866061ba10222f40ad644b3880
memcmahon <mcmahon.meganelizabeth@gmail.com>
2022-01-04 14:12:51 -0700

    Add String Summarizer

---
+++ StringSummarizer/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Summarizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really long text.";
+            var summary = StringUtility.SummarizeText(sentence, 30);
+            Console.WriteLine(summary);
+        }
+    }
+}
