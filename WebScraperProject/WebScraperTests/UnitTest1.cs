ed0902746daea52d7b9f14e5f23694d17d28d0e8
memcmahon <mcmahon.meganelizabeth@gmail.com>
2022-01-10 10:27:42 -0700

    WebScraper: Initial Commit

---
+++ WebScraperProject/WebScraperTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebScraperTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
